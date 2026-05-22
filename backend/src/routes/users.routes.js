dd8a3b8a13b9d3bc274a355fb16322f3d91dc04f
Alvaro <salazares2005@gmail.com>
2026-05-22 17:55:57 -0500

    feat: inicializando el funcionamiento de users

---
+++ backend/src/routes/users.routes.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var router = express.Router();
+
+users.get("/", (req, res) => {
+    res.json({
+
+    })
+});
+
+users.post("/", (req, res) => {
+    res.json({
+        
+    })
+});
+
+module.exports = users;
